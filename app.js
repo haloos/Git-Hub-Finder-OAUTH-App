6bf771400840a65d8d5d503421a80041252fa24c
Jason <haloos@me.com>
2018-12-21 13:20:48 -0500

    show alert message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const github = new Github;
 const ui = new UI;
 
 // Search input
-const searchUser = document.getElementById('searchUser'); 
+const searchUser = document.getElementById('searchUser');
 
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
@@ -12,18 +12,19 @@ searchUser.addEventListener('keyup', (e) => {
   const userText = e.target.value;
 
   if(userText !== ''){
-    // Make http call
-    github.getUser(userText)
-      .then(data => {
-       if(data.profile.message === 'Not Found') {
-          // Show alert
-
-       } else {
-          // Show profile 
-          ui.showProfile(data.profile);
-       }
-      })
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+      }
+    })
   } else {
     // Clear profile
+    ui.clearProfile();
   }
-});
+}); 
--- github.js
+++ github.js
@@ -1,16 +1,16 @@
 class Github {
   constructor() {
-    this.client_id = '21d3f9f37ff02067e771';
-    this.client_secret = 'dab3273c20c4fc9a21c3f540dd4539d9cd6d0ef7';
+    this.client_id = 'd9308aacf8b204d361fd';
+    this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
   }
 
- async getUser(user) {
-   const profileResponse = await fetch(`https://api.github.com/users/${user}?client id=${this.client_id}&client_secret=${this.client_secret}`);
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-   const profile = await profileResponse.json();
+    const profile = await profileResponse.json();
 
-   return {
-    profile
-   }
- }
+    return {
+      profile
+    }
+  }
 }
